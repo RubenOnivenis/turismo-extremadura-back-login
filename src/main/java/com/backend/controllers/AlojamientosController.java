764db60551b8dd0cf0d1d10385da901bf2a0e410
RubenOnivenis <ronivenismunoz.guadalupe@alumnado.fundacionloyola.net>
2021-05-10 21:43:10 +0200

    v0.4 arreglar fallos

--- src/main/java/com/backend/controllers/AlojamientosController.java
+++ src/main/java/com/backend/controllers/AlojamientosController.java
@@ -3,10 +3,8 @@ package com.backend.controllers;
 import com.backend.models.entity.Alojamientos;
 import com.backend.models.services.AlojamientosService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.GeneratedValue;
 import java.util.List;
 
 @RestController @RequestMapping("/api") @CrossOrigin(origins = {"http://localhost:4200"})
@@ -15,7 +13,6 @@ public class AlojamientosController {
     @Autowired
     AlojamientosService alojamientosService;
 
-    @PreAuthorize("hasRole('USER')")
     @GetMapping("/alojamientos")
     public List<Alojamientos> getAlojamientosByName(@PathVariable String nombre){
         return alojamientosService.getAlojamientosByName(nombre);
