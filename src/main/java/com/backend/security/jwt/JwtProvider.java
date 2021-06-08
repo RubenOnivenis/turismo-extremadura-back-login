bb6b9376890ebc3560f737f8c628037d2605b2da
Jaime Avila <javilareyes.guadalupe@alumnado.fundacionloyola.net>
2021-06-08 19:32:06 +0200

    Comentarios

--- src/main/java/com/backend/security/jwt/JwtProvider.java
+++ src/main/java/com/backend/security/jwt/JwtProvider.java
@@ -18,16 +18,20 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/** @Component: Es el estereotipo general y permite anotar un bean para que Spring lo considere uno de sus objetos. **/
 @Component
 public class JwtProvider {
     private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
 
+    /** @Value asigna un valor a la variable, en este caso es el tipo de encriptacion **/
     @Value("${jwt.secret}")
     private String secret;
 
+    /** @Value asigna un valor a la variable, en este caso es el tiempo de expiracion del token **/
     @Value("${jwt.expiration}")
     private int expiration;
 
+    /** Metodo para generar el token, a traves del usuario y sus roles asignados **/
     public String generateToken(Authentication authentication){
         UsuarioPrincipal usuarioPrincipal = (UsuarioPrincipal) authentication.getPrincipal();
         List<String> roles = usuarioPrincipal.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
@@ -40,10 +44,12 @@ public class JwtProvider {
                 .compact();
     }
 
+    /** Metodo para obtener el nombre del usuario del token generado **/
     public String getNombreUsuarioFromToken(String token){
         return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody().getSubject();
     }
 
+    /** Metodo para validar el token generado **/
     public boolean validateToken(String token){
         try {
             Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token);
@@ -62,6 +68,7 @@ public class JwtProvider {
         return false;
     }
 
+    /** Metodo para refrescar el token generado **/
     public String refreshToken(JwtDto jwtDto) throws ParseException {
         JWT jwt = JWTParser.parse(jwtDto.getToken());
         JWTClaimsSet claims = jwt.getJWTClaimsSet();
